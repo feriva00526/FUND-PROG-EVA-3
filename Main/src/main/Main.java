99987a7d4b4f7395caaff6ad75079fbac8ea432c
feriva00526 <143562691+feriva00526@users.noreply.github.com>
2023-11-22 16:48:41 -0700

    Add files via upload

---
+++ Main/src/main/Main.java
@@ -0,0 +1,26 @@
+
+package main;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+     imprimirMensaje ("Hola mundo", 5);
+     
+    Scanner input = new Scanner(System.in);
+    
+    }
+    
+    public static void imprimirMensaje(String mensaje, int cant){
+        for (int i = 0; i < cant; i++) {
+            
+        }
+        System.out.println(mensaje);
+    
+    } 
+    
+    
+}
