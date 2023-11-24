6418382e7c4fdca95a84b89f59cb0ff76f4da86e
feriva00526 <143562691+feriva00526@users.noreply.github.com>
2023-11-24 16:51:39 -0700

    Add files via upload

---
+++ EVA3_5_REFERENCIA/src/eva3_5_referencia/EVA3_5_REFERENCIA.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva3_5_referencia;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_5_REFERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int [] arreglo = new int [10];
+        System.out.println(arreglo);
+    for(int i : arreglo){
+        System.out.println(i);
+    }
+        int i = 10;
+        System.out.println(i);
+    
+        
+    }
+    
+}
