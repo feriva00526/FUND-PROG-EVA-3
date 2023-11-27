293e55254e09c28c8caf4a142bfa23a30552889a
feriva00526 <143562691+feriva00526@users.noreply.github.com>
2023-11-27 16:56:12 -0700

    Add files via upload

---
+++ EVA3_7_OBJETOS/src/eva3_7_objetos/EVA3_7_OBJETOS.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva3_7_objetos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_7_OBJETOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+Persona perso1 = new Persona();
+        System.out.println(perso1);
+        perso1.nombre = "Juan";
+         perso1.apellido = "Perez";
+         perso1.edad = 10;
+           imprimirPersonas(perso1);
+         
+         Persona perso2 = new Persona();
+        System.out.println(perso2);
+        perso2.nombre = "Pedro";
+         perso2.apellido = "Paramo";
+         perso2.edad = 70;
+         imprimirPersonas(perso2);
+    }
+    
+    public static void imprimirPersonas(Persona perso){
+        System.out.println("Direccion: " + perso);
+        System.out.println("Nombre completo " + perso.nombre + "" +perso.apellido);
+        System.out.println("Edad: " + perso.edad );
+    }
+}
+
+class Persona{
+String nombre;
+String apellido;
+int edad;
+
+
+
+}
